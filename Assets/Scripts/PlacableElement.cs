d6963f76fbc1fcdab0bad962e19ad40e5582aaef
Liam <ltconflans@gmail.com>
2026-01-19 17:26:44 +0100

    placement des platforomeesse

---
+++ Assets/Scripts/PlacableElement.cs
@@ -0,0 +1,33 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlacableElement : MonoBehaviour
+{
+    private bool isSelected;
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z+10));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z+10));
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if(hit.transform.position == transform.position)
+                isSelected = !isSelected;
+                Debug.Log("is selected");
+            }
+
+        }
+
+        if (isSelected)
+        {
+            Debug.Log("2");
+            transform.position = mousePosition;
+        }
+        
+    }
+    
+}
