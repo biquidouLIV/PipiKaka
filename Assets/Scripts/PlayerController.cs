61c00b22e5efaac47ebf8158720b4873cd99d761
Hippolyte <jombarthippolyte@gmail.com>
2026-01-19 17:10:25 +0100

    Y A LA FLECHE

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private GameObject arrowParent;
     private Vector2 moveInput;
+    private Vector2 rotationInput = new Vector2(0,1);
     private Rigidbody rb;
-
-    void Awake()
+    
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
@@ -17,9 +19,35 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        rotationInput = context.ReadValue<Vector2>();
+        Debug.Log(rotationInput);
+    }
+        
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            float tempsAppui = (float)context.duration;
+            
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 dir = new (moveInput.x, 0f, 0f);
         rb.MovePosition(rb.position + dir * speed * Time.deltaTime);
+        if (rotationInput.x != 0)
+        {
+            float angle = Mathf.Atan(rotationInput.y / rotationInput.x);
+            if (rotationInput.x > 0)
+                angle += Mathf.PI;
+            Vector3 rot = new (0f, 0f, (angle+ Mathf.PI/2) * 180 / Mathf.PI);
+        
+            Debug.Log(rot);
+            arrowParent.transform.rotation = Quaternion.Euler(rot);
+        }
     }
+    
 }
