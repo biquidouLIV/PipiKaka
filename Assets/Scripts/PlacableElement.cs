bbd4e817a36b5cfb6beec8735101fb041ef6603d
Liam <ltconflans@gmail.com>
2026-01-20 15:50:39 +0100

    omg ont peu pozé les blauks

--- Assets/Scripts/PlacableElement.cs
+++ Assets/Scripts/PlacableElement.cs
@@ -6,32 +6,92 @@ using UnityEngine.InputSystem;
 public class PlacableElement : MonoBehaviour
 {
     private bool isSelected;
+    private Camera mainCamera;
+    private Vector3 offset;
+    private float zDepth = 0f;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        zDepth = transform.position.z;
+    }
+
     private void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z+9));
+
+        if (isSelected && Input.GetKeyDown(KeyCode.R))
+        {
+            transform.Rotate(new Vector3(0, 0, -90));
+        }
+
+
+        if (isSelected)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = mainCamera.WorldToScreenPoint(transform.position).z;
+
+            Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+            transform.position = new Vector3(worldPos.x, worldPos.y, zDepth);
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (isSelected)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-            }
-            
-            
-            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z+10));
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                if(hit.transform.position == transform.position)
-                isSelected = !isSelected;
-            }
+            HandleSelection();
         }
+    }
+
+    private void HandleSelection()
+    {
         if (isSelected)
         {
-            if (Input.GetKeyDown(KeyCode.R))
+            if (CanPlaceObject())
             {
-                transform.Rotate(new Vector3(0,0,-90));
+                DropObject();
             }
-            transform.position = mousePosition;
+            else
+            {
+                Debug.Log("prout");
+            }
         }
+        else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform == transform)
+                {
+                    PickupObject();
+                }
+            }
+        }
+    }
+
+    private bool CanPlaceObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.CompareTag("Platform") && hit.transform != transform)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void PickupObject()
+    {
+        isSelected = true;
+        gameObject.tag = "SelectedPlatform";
+    }
+
+    private void DropObject()
+    {
+        isSelected = false;
+        gameObject.tag = "Platform";
+        transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), zDepth);
     }
 }
